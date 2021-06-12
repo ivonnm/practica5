c1136ab636db4dde1cc006a9b1a874862821a866
ivonnm <66538222+ivonnm@users.noreply.github.com>
2021-06-11 21:31:32 -0500

    ejercicio5

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './articulos.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  articulos: any = null;
+  title = 'proyecto13';
+  constructor(private articulosService: ArticulosService){}
+
+  ngOnInit() {
+    this.articulosService.retornar().subscribe( result => this.articulos = result)
+  }
+  
+}
